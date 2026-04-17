383123716c91adef5e2709056af5252e1623369c
om yengantiwar <omgithub01@gmail.com>
2026-04-17 18:26:57 +0530

    Update habbit_tracker.py

--- habbit_tracker.py
+++ habbit_tracker.py
@@ -2,48 +2,55 @@ import os
 
 tasks = {}
 
+
 if os.path.exists("tasks.txt"):
     with open("tasks.txt", "r") as file:
         for line in file:
             task, status = line.strip().split(",")
-            tasks[task] = True if status == "true" else False
+            if status == "true":
+                tasks[task] = True
+            else:
+                tasks[task] = False
 
 while True:
-    print("______add ur tasks____________")
+    print("---____ ADD YOUR TASKS___----")
     print("1. add task")
     print("2. view tasks")
     print("3. mark task as done")
     print("4. exit")
 
-    choice = input("enter your choice:")
+    choice = input("enter ur choice: ")
 
     if choice == "1":
-        task = input("enter ur task:")
+        task = input("enter your task: ")
         tasks[task] = False
-        print("ur task is added successfully")
+        print("task added")
 
     elif choice == "2":
         if not tasks:
-            print("there is no task to show")
+            print("no tasks")
         else:
-            print("ur task list:")
             for task, status in tasks.items():
-                if status:
-                    print(task, "this task is done")
+                if status == True:
+                    print(task, "- done")
                 else:
-                    print(task, "this task are not done")
+                    print(task, "- not done")
+
     elif choice == "3":
-        task = input("enter the task u want to make it as done:")
+        task = input("enter task name: ")
         if task in tasks:
             tasks[task] = True
-            print("ur task is marked as done")
-            if status != True:
-                print(task,"this tasks are not done")
+            print("marked as done")
+        else:
+            print("task not found")
 
     elif choice == "4":
         with open("tasks.txt", "w") as file:
-            for task,status in tasks.items():
-                file.write(tasks + " " + str(status) + "\n")
-                print("thanks for using our app")
-                break
-    else:        print("invalid input, try again")
+            for task, status in tasks.items():
+                file.write(task + "," + str(status).lower() + "\n")
+
+        print("thanks")
+        break
+
+    else:
+        print("invalid input")
